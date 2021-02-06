32815989a4c33a4cad19a33a295c866695066089
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2021-02-06 08:12:31 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -89,7 +89,9 @@ async def echo(bot, update):
             "--no-warnings",
             "--youtube-skip-dash-manifest",
             "-j",
-            url
+            url,
+            "--geo-bypass-country",
+            "IN"
         ]
     if "hotstar" in url:
         command_to_exec.append("--geo-bypass-country")
@@ -238,30 +240,23 @@ async def echo(bot, update):
 
         thumbnail = Config.DEF_THUMB_NAIL_VID_S
         thumbnail_image = Config.DEF_THUMB_NAIL_VID_S
-        thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+        if "thumbnail" in response_json:
+               if response_json["thumbnail"] is not None:
+                   thumbnail = response_json["thumbnail"]
+                   thumbnail_image = response_json["thumbnail"]
+    
+        #thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
 
-        if not os.path.exists(thumb_image_path):
-            mes = await thumb(update.from_user.id)
-            if mes != None:
-                m = await bot.get_messages(update.chat.id, mes.msg_id)
-                await m.download(file_name=thumb_image_path)
-                thumb_image_path = thumb_image_path
-            else:
-                if "thumbnail" in response_json:
-                    if response_json["thumbnail"] is not None:
-                        thumbnail = response_json["thumbnail"]
-                        thumbnail_image = response_json["thumbnail"]
-                thumb_image_path = DownLoadFile(
-                    thumbnail_image,
-                    Config.DOWNLOAD_LOCATION + "/" +
-                    str(update.from_user.id) + ".jpg",
-                    Config.CHUNK_SIZE,
-                    None,  # bot,
-                    Translation.DOWNLOAD_START,
-                    update.message_id,
-                    update.chat.id
-                )
-        await intmsg.delete()
+            thumb_image_path = DownLoadFile(
+                thumbnail_image,
+                Config.DOWNLOAD_LOCATION + "/" +
+                str(update.from_user.id) + ".jpg",
+                Config.CHUNK_SIZE,
+                None,  # bot,
+                Translation.DOWNLOAD_START,
+                update.message_id,
+                update.chat.id
+            )        await intmsg.delete()
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
